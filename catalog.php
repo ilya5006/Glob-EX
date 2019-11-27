f260c900f30fc63b1f79867c994b17d27894235a
Alexander <mrreads@yandex.com>
2019-11-27 15:39:07 +0300

    catalog page

---
+++ catalog.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Каталог 2 уровня </title>
+    <link rel="stylesheet" href="./resource/css/normalize.css">
+    <link rel="stylesheet" href="./resource/css/base.css">
+    <link rel="stylesheet" href="./resource/css/header.css">
+    <link rel="stylesheet" href="./resource/css/banner.css">
+    <link rel="stylesheet" href="./resource/css/catalog_two.css">
+    <link rel="stylesheet" href="./resource/css/footer.css">
+    <script src="./resource/js/header.js" defer></script>
+    <script src="./resource/js/banner.js" defer></script>
+    <script src="./resource/js/slider.js" defer></script>  
+</head>
+<body>
+
+    <?php require(__DIR__ . '/view/header.php'); ?>
+
+    <?php require(__DIR__ . '/view/banner.php'); ?>
+
+    <?php require(__DIR__ . '/view/catalog_two.php'); ?>
+
+
+    <?php require(__DIR__ . '/view/footer.php'); ?>
+
+</body>
+</html>
---
+++ view/catalog_two.php
@@ -0,0 +1,67 @@
+<!-- КАТАЛОГ 2 УРОВНЯ-->
+<div class="content">
+    <div class="catalog_two">
+        <div class="cat_fold"> <p>Каталог</p> <p> <span> &frasl; Канцелярские товары </span> </p></div>
+        <h2> Канцелярское чточто</h2>
+        <div class="catalog_list">
+            
+            <div class="catalog">
+                <img src="./resource/img/catalog/image1.png" alt="cat imgs">
+                <div class="cat">
+                    <p> Бумажная продукция</p>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                </div>
+            </div>
+
+            <div class="catalog">
+                <img src="./resource/img/catalog/image1.png" alt="cat imgs">
+                <div class="cat">
+                    <p> Бумажная продукция</p>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                </div>
+            </div>
+            <div class="catalog">
+                <img src="./resource/img/catalog/image1.png" alt="cat imgs">
+                <div class="cat">
+                    <p> Бумажная продукция</p>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                </div>
+            </div>
+            <div class="catalog">
+                <img src="./resource/img/catalog/image1.png" alt="cat imgs">
+                <div class="cat">
+                    <p> Бумажная продукция</p>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                </div>
+            </div>
+            <div class="catalog">
+                <img src="./resource/img/catalog/image1.png" alt="cat imgs">
+                <div class="cat">
+                    <p> Бумажная продукция</p>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                    <a href="#">АЛЬБОМЫ ДЛЯ РИСОВАНИЯ</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- КОНЕЦ КАТАЛОГА ВТОРОГО УРОВНЯ-->
