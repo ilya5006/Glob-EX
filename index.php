8e3cd2ab8e7b64a4b9d70bee2c9032452a4a12a2
Alexander <mrreads@yandex.com>
2019-11-27 14:48:28 +0300

    view / resources

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Главная страница </title>
+    <link rel="stylesheet" href="./resource/css/normalize.css">
+    <link rel="stylesheet" href="./resource/css/base.css">
+    <link rel="stylesheet" href="./resource/css/header.css">
+    <link rel="stylesheet" href="./resource/css/banner.css">
+    <link rel="stylesheet" href="./resource/css/scroll.css">
+    <link rel="stylesheet" href="./resource/css/footer.css">
+</head>
+<body>
+    <?php require(__DIR__ . '/view/header.php'); ?>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,69 @@
+<header class="header">
+    <div class="content">
+        <div class="menu">
+            <a href="#"><img src="./resource/img/logo.svg" alt="logo"></a>
+            <ul>
+                <li> <a href="#">Доставка</a> </li>
+                <li> <a href="#">Оплата</a> </li>
+                <li> <a href="#">Контакт</a> </li>
+                <li> <a href="#">Задать вопрос</a> </li>
+                <li> <a href="#">О компании</a> </li>
+                <li> <a href="#" class="tel">8-495-380-42-88</a> </li>
+            </ul>
+        </div>
+        <div class="nav">
+            <button class="catalog" id="catalog"> <span>|||</span> КАТАЛОГ ТОВАРОВ</button>
+
+            <div class="expand-catalog hide">
+                <a class="cat_1" href="#"> <img src="./resource/img/percent.svg">
+                    <p> Акции и скидки </p>
+                </a>
+                <a class="cat_1" href="#"> <img src="./resource/img/clip.svg">
+                    <p>Концелярии и товары</p><span>&#62;</span>
+                </a>
+                <div class='expand-cat_2 hide'>
+                    <ul>
+                        <li><span>1</span></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                    </ul>
+                </div>
+                <a class="cat_1" href="#"> <img src="./resource/img/bucket.svg"><p>Хозтовары</p><span>&#62;</span></a>
+                <div class='expand-cat_2 hide'>
+                    <ul>
+                        <li><span>2</span></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                    </ul>
+                </div>
+                <a class="cat_1" href="#"> <img src="./resource/img/furniture.svg"><p>Мебель</p><span>&#62;</span></a>
+                <div class='expand-cat_2 hide'>
+                    <ul>
+                        <li><span>3</span></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                        <li><a href="#">asdasd</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <form action="#">
+                <input type="search" placeholder="Поиск...">
+                <button> <img src="./resource/img/search.svg" alt="search"> </button>
+            </form>
+            <ul>
+                <li><a href="#"><img src="./resource/img/favourite.svg" alt="favorite"></a></li>
+                <li><a href="#"><img src="./resource/img/cart.svg" alt="cart"></a></li>
+                <li class="account"><a href="#"><img src="./resource/img/account.svg" alt="account"><p>Войти</p></a></li>
+            </ul>
+        </div>
+    </div>
+</header>
