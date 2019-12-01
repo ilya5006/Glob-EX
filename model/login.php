48b85d28c6bbdabaccfa7b0a35e8f4af233b7b5a
ilya5006 <samarcev.ilya@mail.ru>
2019-12-01 13:04:34 +0300

    Login tocken

--- model/login.php
+++ model/login.php
@@ -2,13 +2,14 @@
     $loginEntered = mysqli_real_escape_string($link, $_GET['login']);
     $passwordEntered = mysqli_real_escape_string($link, $_GET['password']);
 
-    $loginPasswordDbQuery = mysqli_query($link, "SELECT login, password, id_user FROM users_all WHERE login = '$loginEntered'");
-    $loginPasswordDb = mysqli_fetch_row($loginPasswordDbQuery);
-    $passwordDb = $loginPasswordDb[1];
+    $loginPasswordIdDbQuery = mysqli_query($link, "SELECT login, password, id_user FROM users_all WHERE login = '$loginEntered'");
+    $loginPasswordIdDb = mysqli_fetch_row($loginPasswordIdDbQuery);
+    $passwordDb = $loginPasswordIdDb[1];
+    $idDb = $loginPasswordIdDb[2];
 
     if (password_verify($passwordEntered, $passwordDb))
     {
-        // Пользователь успешно вошёл
+        setcookie("isLogin", md5($idDb), time() + 60*60*24*7); // 7 дней
     }
     else
     {
