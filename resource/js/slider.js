cc91ea482404b734c7b74b4042562f00858ffca7
Alexander <mrreads@yandex.com>
2019-12-03 14:56:37 +0300

    gray buttom scroll, when dont scroll cant move

--- resource/js/slider.js
+++ resource/js/slider.js
@@ -3,15 +3,27 @@
 var elmentWidth;
 var scrolls = document.querySelectorAll('.slider_header');
 scrolls.forEach(function (element) 
-{
-    element.querySelector('.slide_right').addEventListener('click', function () 
+{        
+    if (parseInt(element.nextElementSibling.scrollLeft) == 0)
     {
-        elmentWidth = element.nextElementSibling.children[0].offsetWidth + 20;
-        element.nextElementSibling.scrollTo(element.nextElementSibling.scrollLeft + elmentWidth, 0);
-    });
+        element.querySelectorAll('div')[0].style.filter = 'grayscale(1)';
+    }
     element.querySelector('.slide_left').addEventListener('click', function () 
     {
         elmentWidth = element.nextElementSibling.children[0].offsetWidth + 20;
         element.nextElementSibling.scrollTo(element.nextElementSibling.scrollLeft - elmentWidth, 0);
+
+        (parseInt(element.nextElementSibling.scrollLeft) == 0) ? element.querySelectorAll('div')[0].style.filter = 'grayscale(1)' :  element.querySelectorAll('div')[0].style.filter = 'grayscale(0)';
+        (parseInt(element.nextElementSibling.scrollLeft) == parseInt(element.nextElementSibling.scrollWidth) - parseInt(element.nextElementSibling.clientWidth)) ? element.querySelectorAll('div')[1].style.filter = 'grayscale(1)' :  element.querySelectorAll('div')[1].style.filter = 'grayscale(0)';
     });
+    element.querySelector('.slide_right').addEventListener('click', function () 
+    {
+        elmentWidth = element.nextElementSibling.children[0].offsetWidth + 20;
+        element.nextElementSibling.scrollTo(element.nextElementSibling.scrollLeft + elmentWidth, 0);
+
+        (parseInt(element.nextElementSibling.scrollLeft) == 0) ? element.querySelectorAll('div')[0].style.filter = 'grayscale(1)' :  element.querySelectorAll('div')[0].style.filter = 'grayscale(0)';
+        (parseInt(element.nextElementSibling.scrollLeft) == parseInt(element.nextElementSibling.scrollWidth) - parseInt(element.nextElementSibling.clientWidth)) ? element.querySelectorAll('div')[1].style.filter = 'grayscale(1)' :  element.querySelectorAll('div')[1].style.filter = 'grayscale(0)';
+        
+    });
+
 });
