78f4f491626ca49d5b064671b92fd00ebc51d9d6
Alexander <mrreads@yandex.com>
2019-12-04 17:41:05 +0300

    cart adaptive

--- view/cart.php
+++ view/cart.php
@@ -3,11 +3,11 @@
         <p> Главная </p>
         <p><span> &#92; Корзина </span></p>
     </div>
+    <h2> Корзина </h2>
     <div class="sliders">
         <a class="cart active"> Моя корзина </a>
         <a class="favourite"> Отложенные товары </a>
     </div>
-
     <div class="cart-block">
         <div class="items">
             <div class="product">
@@ -85,6 +85,22 @@
                     <img src="./resource/img/icons/favourite.svg" alt="fav">
                 </div>
             </div>
+
+            <h2> СПОСОБ ДОСТАВКИ </h2>
+            <div class="delivery">
+                <div class="delivery-type">
+                    <p class="type-deliv active">Доставка</p>
+                    <p class="type-pickup">Самовызов</p>
+                </div>
+                
+            </div>
+
+            <h2> СПОСОБ ОПЛАТЫ </h2>
+            <div class="pay-type">
+                <label class="container" id="cash"> <p> НАЛИЧНЫМИ ПРИ ПОЛУЧЕНИИ </p> <input type="radio" name="pay_type" > <span class="checkmark"></span> </label>
+                <label class="container" id="cart"> <p> ОПЛАТА КАРТОЙ ПРИ ПОЛУЧЕНИИ</p> <input type="radio" name="pay_type"  > <span class="checkmark"></span> </label>
+            </div>
+
         </div>
         <div class="act">
             <p class='act-itog'> ИТОГ <span>8 200</span> </p>
