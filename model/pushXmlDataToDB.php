aa2c3938f974974f3f703b04062b9edc737f213c
ilya5006 <samarcev.ilya@mail.ru>
2019-12-05 21:16:51 +0300

    Almost working xml data pushing file

--- model/pushXmlDataToDB.php
+++ model/pushXmlDataToDB.php
@@ -2,7 +2,6 @@
     require_once './xmlparser.php';
     require_once './connection.php';
 
-    
     echo '<pre>';
         echo var_export($xmlParseData);
     echo '</pre>';
@@ -44,31 +43,84 @@
         $description = $data['description'];
         $icon = $data['icon'];
         
-        $image1 = str_replace('\\', '/', $image1);
-        $image2 = str_replace('\\', '/', $image2);
+        $image1 = str_replace('\\', '/', $image1); // Потому что MySQL экранирует символ \
+        $image2 = str_replace('\\', '/', $image2); // Потому что MySQL экранирует символ \
 
-        $mysqli->query("REPLACE INTO partitions SET id_partition = '$idPartition', top_id = '$topId', name = '$name', technical_name = '$technicalName', sort = '$sort',
-                                                    banner = '$banner', image1 = '$image1', image2 = '$image2', description = '$description', icon = '$icon'");
+        $technicalName = str_replace('\'', '', $technicalName); // Потому что MySQL экранирует символ '
 
-        echo '<p>' . 'Name: '. $name . '</p>';
-        echo '<p>' . 'Technical name: '. $technicalName . '</p>';
-        echo '<p>' . 'Id: '. $idPartition . '</p>';
-        echo '<p>' . 'Top id: '. $topId . '</p>';
-        echo '<p>' . 'Sort: '. $sort . '</p>';
-        echo '<p>' . 'Banner: '. $banner . '</p>';
-        echo '<p>' . 'Image1: '. $image1 . '</p>';
-        echo '<p>' . 'Image2: '. $image2 . '</p>';
-        echo '<p>' . 'Description: '. $description . '</p>';
-        echo '<p>' . 'Icon: '. $icon . '</p>';
+        $query = "REPLACE INTO partitions SET id_partition = '$idPartition', top_id = '$topId', name = '$name', technical_name = '$technicalName', sort = '$sort', 
+                                              banner = '$banner', image1 = '$image1', image2 = '$image2', description = '$description', icon = '$icon'";
 
-        echo '<hr>';
+        if ($topId === '')
+            $query = str_replace("top_id = '$topId'", 'top_id = NULL', $query);
 
-        // if (isset($data['specs']))
-        // {
-        //     foreach($data['specs'] as $key => $idSpec)
-        //     {
-        //         $mysqli->query("REPLACE INTO partition_specs SET id_partition = '$idPartition', id_spec = '$idSpec'");
-        //     }
-        // }
+        $mysqli->query($query);
+
+        if (isset($data['specs']))
+        {
+            foreach($data['specs'] as $key => $idSpec)
+            {
+                //$mysqli->query("REPLACE INTO partition_specs SET id_partition = '$idPartition', id_spec = '$idSpec'");
+                $mysqli->query("INSERT IGNORE INTO partition_specs SET id_partition = '$idPartition', id_spec = '$idSpec'");
+            }
+        }
+    }
+
+    foreach ($xmlParseData['nomeklatura'] as $idGood => $data)
+    {
+        $name = $data['name'];
+        $technicalName = $data['technical_name'];
+        $topId = $data['top_id'];
+        $sort = $data['sort'];
+        $brand = $data['brand'];
+        $idCountry = $data['country'];
+        $quantity = $data['quantity'];
+        $unit = $data['unit'];
+        $price = $data['price'];
+        $oldPrice = $data['old_price'];
+        $discount = $data['discount'];
+        $image1 = $data['image1'];
+        $image2 = $data['image2'];
+        $image3 = $data['image3'];
+        $image4 = $data['image4'];
+        $image5 = $data['image5'];
+        $description = $data['description'];
+        
+        $image1 = str_replace('\\', '/', $image1); // Потому что MySQL экранирует символ \
+        $image2 = str_replace('\\', '/', $image2); // Потому что MySQL экранирует символ \
+        $image3 = str_replace('\\', '/', $image3); // Потому что MySQL экранирует символ \
+        $image4 = str_replace('\\', '/', $image4); // Потому что MySQL экранирует символ \
+        $image5 = str_replace('\\', '/', $image5); // Потому что MySQL экранирует символ \
+
+        $technicalName = str_replace('\'', '', $technicalName); // Потому что MySQL экранирует символ '
+
+        $query = "REPLACE INTO goods SET id_good = '$idGood', id_country = '$idCountry', id_brand = '$brand', top_id = '$topId', name = '$name',
+                                         sort = '$sort', price = '$price', old_price = '$oldPrice', discount = '$discount', unit = '$unit', 
+                                         image1 = '$image1', image2 = '$image2', image3 = '$image3', 
+                                         image4 = '$image4', image5 = '$image5', quantity = '$quantity', description = '$description'";
+
+        if (gettype($oldPrice) === 'NULL')
+            $query = str_replace("old_price = '$oldPrice'", 'old_price = NULL', $query);
+
+        if (gettype($discount) === 'NULL')
+            $query = str_replace("discount = '$discount'", 'discount = NULL', $query);
+
+        if ($brand === '')
+            $query = str_replace("id_brand = '$brand'", 'id_brand = NULL', $query);
+
+        if ($country === '')
+            $query = str_replace("id_country = '$country'", 'id_country = NULL', $query);
+
+        $mysqli->query($query);
+
+
+        if (isset($data['specs']))
+        {
+            foreach($data['specs'] as $idSpec => $value)
+            {
+                // $mysqli->query("REPLACE INTO good_specs SET id_good = '$idGood', id_spec = '$idSpec', value = '$value'");
+                $mysqli->query("INSERT IGNORE INTO good_specs SET id_good = '$idGood', id_spec = '$idSpec', value = '$value'");
+            }
+        }
     }
 ?>
