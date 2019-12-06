d1265c3cdb6612b5f00fb181ca3e8d2488d62b37
Alexander <mrreads@yandex.com>
2019-12-06 14:43:56 +0300

    example for load image from FTP (with base64 encde)

--- view/sliderBrands.php
+++ view/sliderBrands.php
@@ -1,3 +1,8 @@
+<?php
+
+$img = base64_encode(file_get_contents('ftp://user587s:CgIc6Wbt@user587s.beget.tech/Data/Картинки и баннеры/Логотипы/1.jpg'));
+?>
+
 <div class="content">
     <div class="slider_header">
         <h2> Бренды </h2>
@@ -5,7 +10,7 @@
         <div class="slide_right slide_anim"> <span>&#62;</span> </div>
     </div>
     <div class="scroll">
-        <a href="#" class="brand"><img src="./resource/img/brands/1.jpg"></a>
+        <a href="#" class="brand"><img src="data:image/png;base64,<? echo $img; ?>"></a>
         <a href="#" class="brand"><img src="./resource/img/brands/1.jpg"></a>
         <a href="#" class="brand"><img src="./resource/img/brands/1.jpg"></a>
         <a href="#" class="brand"><img src="./resource/img/brands/1.jpg"></a>
