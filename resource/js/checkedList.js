887e3a4541e906c63ad11204990522653cd6cd31
Alexander <mrreads@yandex.com>
2019-12-06 22:07:24 +0300

    js

--- resource/js/checkedList.js
+++ resource/js/checkedList.js
@@ -1,16 +1,31 @@
 ﻿let checkedProducts;
+let allProducts = document.querySelectorAll('.product');
 let allChecked = document.querySelectorAll('.product > .container input:checked');
 let allCheckbox = document.querySelectorAll('.product > .container input[type=checkbox');
+
 allCheckbox.forEach(function(element)
 {
     element.addEventListener('change', function()
     {
-        allChecked = document.querySelectorAll('.product > .container input:checked');
-        checkedProducts = [];
-        allChecked.forEach(function(elem)
-        {
-            checkedProducts.push(elem.parentElement.parentElement.querySelector('.article').textContent);
-        });
-        console.log(checkedProducts);
+        update();
+    });
+});
+
+function update()
+{
+    allChecked = document.querySelectorAll('.product > .container input:checked');
+    checkedProducts = [];
+    allChecked.forEach(function(elem)
+    {
+        checkedProducts.push([elem.parentElement.parentElement.querySelector('.article').textContent, elem.parentElement.parentElement.querySelector('.product-count').value]);
+    });
+    console.log(checkedProducts);
+}
+
+allProducts.forEach(function(element)
+{
+    element.querySelector('input.product-count').addEventListener('change', function()
+    {
+        update();
     });
 });
