baba4c11f2cf4e118d24a897365555427ed6d223
Alexnder <mrreads@yandex.com>
2019-12-14 19:43:23 +0300

    cart price and value

--- view/cart.php
+++ view/cart.php
@@ -21,31 +21,46 @@ $brands = $xmlParseData['brands'];
         <div class="items">
 
         <?php
-            
-            $result = $mysqli->query("SELECT `id_product` FROM `user-cart` WHERE id_user = $idUser;");
+            $cost = 0;
+            $fullCost = 0;
+            $discount = 0;
+
+            $result = $mysqli->query("SELECT * FROM `user-cart` WHERE id_user = $idUser;");
             $count = $mysqli->query("SELECT COUNT(*) FROM `user-cart` WHERE id_user = $idUser")->fetch_row()[0];
 
             if ($count > 0)
             {
-                while($idProduct = $result->fetch_assoc()['id_product'])
+                while($idProduct = $result->fetch_assoc())
                 {
-                    if ($productData[$idProduct]['quantity'] > 0) 
-                    { 
+                    if ($productData[$idProduct['id_product']]['quantity'] > 0) 
+                    {
+                        $cost +=  $productData[$idProduct['id_product']]['price'] * $idProduct['product_count'];
+
+                        if ($productData[$idProduct['id_product']]['old_price']) 
+                        {
+                            $fullCost +=  $productData[$idProduct['id_product']]['old_price'] * $idProduct['product_count']; 
+                            $discount +=  ($productData[$idProduct['id_product']]['old_price'] - $productData[$idProduct['id_product']]['price']) * $idProduct['product_count'];
+                        }
+                        else 
+                        {
+                            $fullCost +=  $productData[$idProduct['id_product']]['price'] * $idProduct['product_count']; 
+                        }
+
                         echo '<div class="product">';
-                        $img = str_replace('ftp://37.140.192.146', './../', $productData[$idProduct]['image1']);
+                        $img = str_replace('ftp://37.140.192.146', './../', $productData[$idProduct['id_product']]['image1']);
                             echo '<img class="product-image" src="'.$img.'" alt="">';
                             echo '<div class="product-info">';
-                                echo '<p class="product-name">'.$productData[$idProduct]['name'].'</p>';
-                                echo '<p class="article" style="display: none;">'.$productData[$idProduct]['article'].'</p>';
-                                echo '<p class="id" style="display: none;">'.$productData[$idProduct]['id'].'</p>';
+                                echo '<p class="product-name">'.$productData[$idProduct['id_product']]['name'].'</p>';
+                                echo '<p class="article" style="display: none;">'.$productData[$idProduct['id_product']]['article'].'</p>';
+                                echo '<p class="id" style="display: none;">'.$productData[$idProduct['id_product']]['id'].'</p>';
                                 echo '<div class="product-detail">';
-                                    echo '<p class="available">есть в наличии <span class="available-count">'.$productData[$idProduct]['quantity'].'</span> </p>';
-                                if ($brands[$productData[$idProduct]['brand']]['name']) { echo '<p>БРЕНД: <a href="#">'.$brands[$productData[$idProduct]['brand']]['name'].'</a></p>'; }
+                                    echo '<p class="available">есть в наличии <span class="available-count">'.$productData[$idProduct['id_product']]['quantity'].'</span> </p>';
+                                if ($brands[$productData[$idProduct['id_product']]['brand']]['name']) { echo '<p>БРЕНД: <a href="#">'.$brands[$productData[$idProduct['id_product']]['brand']]['name'].'</a></p>'; }
                                 else { echo '<p>БРЕНД: <a> ОТСУСТВУЕТ </a></p>'; }
                                 echo '</div>';
                             echo '</div>';
-                            echo '<p class="product-price">'.$productData[$idProduct]['price'].'</p>';
-                            echo '<input class="product-count" type="number" name="" id="" min="1" max="'.$productData[$idProduct]['quantity'].'" value="1">';
+                            echo '<p class="product-price">'.$productData[$idProduct['id_product']]['price'].'</p>';
+                            echo '<input class="product-count" type="number" name="" id="" min="1" max="'.$productData[$idProduct['id_product']]['quantity'].'" value="'.$idProduct['product_count'].'">';
                             echo '<div class="fav-button">';
                                 echo '<img src="./resource/img/icons/favourite.svg" alt="fav">';
                             echo '</div>';
@@ -93,12 +108,13 @@ $brands = $xmlParseData['brands'];
                             </label>
                         </div>
 
+
                     </div>
                     <div class="act">
-                        <p class='act-itog'> ИТОГ <span>8 200</span> </p>
+                        <?php echo '<p class="act-itog"> ИТОГ <span>'.$cost.'</span> </p>'; ?>
                         <hr>
-                        <p class='act-tovari'> товары <span>8 200</span> </p>
-                        <p class='act-skidka'> скидка <span>8 200</span> </p>
+                        <?php echo '<p class="act-tovari"> товары <span>'.$fullCost.'</span> </p>'; ?>
+                        <?php echo '<p class="act-skidka"> скидка <span>'.$discount.'</span> </p>'; ?>
                         <!-- <p class='act-dostavka'> доставка <span>200</span> </p> -->
                         <input class="order-button" type="submit" value="ОФОРМИТЬ ЗАКАЗ">
                     </div>
