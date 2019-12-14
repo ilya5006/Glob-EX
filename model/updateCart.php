2a64cb65da9bcb7cb0e61236766f3663a2a9fa94
Alexnder <mrreads@yandex.com>
2019-12-14 17:50:28 +0300

    cart

---
+++ model/updateCart.php
@@ -0,0 +1,9 @@
+<?php
+    require_once './connection.php';
+
+    $idUser = (int)$_COOKIE['isLogin'];
+    $idProduct = $mysqli->escape_string($_POST['id_product']);
+    $productCount = $mysqli->escape_string($_POST['product_count']);
+
+    $mysqli->query("UPDATE `user-cart` SET `product_count` = $productCount WHERE id_user = $idUser AND id_product = $idProduct;");
+?>
--- resource/js/cart.js
+++ resource/js/cart.js
@@ -5,18 +5,28 @@ products.forEach(function(element)
 {
     element.querySelector('input.product-count').addEventListener('change', function()
     {
-        update();
+        update(element.querySelector('.id').textContent, element.querySelector('input[type=number]').value);
     });
 });
 
-function update()
+function update(id_product, product_count)
 {
+    let cartUpdate = new FormData();
+    cartUpdate.append('id_product', id_product);
+    cartUpdate.append('product_count', product_count);
+    
+    let connectionCart = fetch('../../model/updateCart.php', 
+    {
+        method: 'POST',
+        body: cartUpdate
+    });
+    connectionCart.then((result) =>
+    {
+        result.text().then(result => console.log(result));
+    });
     productInCart = [];
     products.forEach(function(element)
     {   
-        productInCart.push([element.querySelector('.article').textContent, element.querySelector('input.product-count').value]);
+        element.querySelector('.article').textContent, element.querySelector('input.product-count').value
     });
-    console.log(productInCart);
 }
-
-update();
