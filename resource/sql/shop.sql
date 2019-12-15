b29be8917722dd390031049647e9201a576853ec
Alexnder <mrreads@yandex.com>
2019-12-16 00:13:48 +0300

    update DB

--- resource/sql/shop.sql
+++ resource/sql/shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 14 2019 г., 17:33
+-- Время создания: Дек 16 2019 г., 00:13
 -- Версия сервера: 10.3.13-MariaDB-log
 -- Версия PHP: 7.3.9
 
@@ -34,13 +34,6 @@ CREATE TABLE `user-cart` (
   `product_count` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Дамп данных таблицы `user-cart`
---
-
-INSERT INTO `user-cart` (`id_user`, `id_product`, `product_count`) VALUES
-(8, 11, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -66,7 +59,7 @@ CREATE TABLE `users_all` (
   `email` varchar(100) NOT NULL,
   `phone_number` varchar(20) NOT NULL,
   `work_number` varchar(20) DEFAULT NULL,
-  `address` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
   `additional_address` varchar(255) DEFAULT NULL,
   `mailing` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -76,14 +69,7 @@ CREATE TABLE `users_all` (
 --
 
 INSERT INTO `users_all` (`id_user`, `role`, `fio`, `password`, `email`, `phone_number`, `work_number`, `address`, `additional_address`, `mailing`) VALUES
-(1, 0, 'Да Да Да', '$2y$10$58adI2iM5PEp7y2pZcZHJeboEvDkbhFEmjb/vmG2dcLLnWBhtvciu', 'dada@dada.dada', '88005553535', NULL, 'ФЫВА', '', 0),
-(2, 0, 'Яяя Ааа Ддд', '$2y$10$ZOcqE7WwwClqNHMNMLmKHuHShokrUCYvdqZIpy.rNIZ8D43G5vmAW', 'yaya@yaya.yaya', '85025698756', NULL, 'ФЫВА', '', 0),
-(3, 0, 'Яяя Ааа Aaa', '$2y$10$HMoWHpOkdETsiSH2unzCtuyEULHXaHcHu.Eydx9az5nu6WG9VIdSC', 'yaya@yaya.yaya', '85025698756', NULL, 'ФЫВА', '', 1),
-(4, 1, 'Яяя Ааа Aaa', '$2y$10$ZdAidiOGlOv77fSYRAIw.eNHYZA2jwuapeK.nQkFoRz.kAp5JfKke', 'yaya@yaya.yaya', '85025698756', NULL, 'ФЫВА', '', 1),
-(5, 1, 'Яяя Ааа Aaa', '$2y$10$eJqj1a5ZlMmpXX3s1IB/seU3LGyLmdXyzC4ENyRD/GZ0uIcf/CSEO', 'yaya@yaya.yaya', '85025698756', NULL, 'ФЫВА', '', 0),
-(6, 1, '123', '$2y$10$OmGVARGpFTRfJKwMk/kOee3l9jvydKfAB2Bbz8omlXsqlBJORelpa', '123@123.123', '123', NULL, 'ФЫВА', '', 0),
-(7, 1, 'AAa AAa AaA', '$2y$10$WN6n42g/nlMPBy/ynF9XEOQ1lRyLoIQXpNIPgt7Q2JMS9MsAEmynG', 'email@email.email', '8 999 555 12 22', NULL, 'ФЫВА', '', 0),
-(8, 0, 'Манисов Александр Ильйч', '$2y$10$bCZq4EDwTEcqOtypskNezO1GCtnBdeA9sbyLvuD64rH0u35PNe4Ye', 'mrreads@yandex.com', '+79259294570', NULL, 'ФЫВА', '', 0);
+(1, 0, 'Александр Манисов', '$2y$10$AZMQJL4O9LhR5RKdDq1Lm.zmbkGV2.w9Pgl6pVpq8x3QY.OIZDsYS', 'test@test.ru', '+7999999999', NULL, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -94,16 +80,6 @@ INSERT INTO `users_all` (`id_user`, `role`, `fio`, `password`, `email`, `phone_n
 CREATE TABLE `users_entities` (
   `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `users_entities`
---
-
-INSERT INTO `users_entities` (`id_user`) VALUES
-(4),
-(5),
-(6),
-(7);
 
 -- --------------------------------------------------------
 
@@ -120,10 +96,7 @@ CREATE TABLE `users_individuals` (
 --
 
 INSERT INTO `users_individuals` (`id_user`) VALUES
-(1),
-(2),
-(3),
-(8);
+(1);
 
 --
 -- Индексы сохранённых таблиц
@@ -167,7 +140,7 @@ ALTER TABLE `users_individuals`
 -- AUTO_INCREMENT для таблицы `users_all`
 --
 ALTER TABLE `users_all`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -177,25 +150,25 @@ ALTER TABLE `users_all`
 -- Ограничения внешнего ключа таблицы `user-cart`
 --
 ALTER TABLE `user-cart`
-  ADD CONSTRAINT `user-cart_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users_all` (`id_user`);
+  ADD CONSTRAINT `user-cart_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users_all` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `user-favoutite`
 --
 ALTER TABLE `user-favoutite`
-  ADD CONSTRAINT `user-favoutite_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users_all` (`id_user`);
+  ADD CONSTRAINT `user-favoutite_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users_all` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `users_entities`
 --
 ALTER TABLE `users_entities`
-  ADD CONSTRAINT `users_entities_fk0` FOREIGN KEY (`id_user`) REFERENCES `users_all` (`id_user`);
+  ADD CONSTRAINT `users_entities_fk0` FOREIGN KEY (`id_user`) REFERENCES `users_all` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `users_individuals`
 --
 ALTER TABLE `users_individuals`
-  ADD CONSTRAINT `users_individuals_fk0` FOREIGN KEY (`id_user`) REFERENCES `users_all` (`id_user`);
+  ADD CONSTRAINT `users_individuals_fk0` FOREIGN KEY (`id_user`) REFERENCES `users_all` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
