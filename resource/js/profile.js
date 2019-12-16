9efde48dbf7657eb5c4f9649339dc73ecfdcb943
Alexnder <mrreads@yandex.com>
2019-12-16 18:06:55 +0300

    profile update

---
+++ resource/js/profile.js
@@ -0,0 +1,12 @@
+﻿let additionalInputs = document.querySelectorAll('.additionalAdress');
+let additionalAdressButton = document.querySelector('#addAddress');
+
+counter = 0;
+additionalAdressButton.addEventListener('click', function()
+{
+    if (counter < additionalInputs.length)
+    {
+        additionalInputs[counter].style.display = 'flex';
+        counter++;
+    }
+});
