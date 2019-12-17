547629d171f3dced11519f0b2bdf40557455be20
Alexnder <mrreads@yandex.com>
2019-12-18 01:48:19 +0300

    minor fix

--- products.php
+++ products.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); 
-require_once __DIR__ . './model/xmlparser.php';
+require_once __DIR__ . '/model/xmlparser.php';
 $idCategory = $_GET['id'];
 $title = $xmlParseData['partitions'][$idCategory]['name'];
 ?>
