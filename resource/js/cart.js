9ec2a10739a533910d80753406716eb5e60450dc
Alexnder <mrreads@yandex.com>
2019-12-17 12:42:47 +0300

    fix visual bug + remove debug

--- resource/js/cart.js
+++ resource/js/cart.js
@@ -82,7 +82,7 @@ function ajax()
                             }
                             else
                             {
-                                showMessaage(result);
+                                //showMessaage(result);
                             }
                         });
                     });
