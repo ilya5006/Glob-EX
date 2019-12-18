080a8d35877aa9f75ffb4d3cb041eb884fa4ae27
Alexnder <mrreads@yandex.com>
2019-12-18 22:04:47 +0300

    orders page template

---
+++ orders.php
@@ -0,0 +1,45 @@
+<? 
+
+session_start(); 
+
+if (empty($_COOKIE['isLogin'])) { header('Location: ' . $_SERVER['HTTP_REFERER']); }
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Ваши заказы </title>
+    <link rel="shortcut icon" href="./resource/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./resource/css/normalize.css">
+    <link rel="stylesheet" href="./resource/css/base.css">
+    <link rel="stylesheet" href="./resource/css/header.css">
+    <link rel="stylesheet" href="./resource/css/delivery.css">
+    <link rel="stylesheet" href="./resource/css/cart.css">
+    <link rel="stylesheet" href="./resource/css/footer.css">
+    <script src="./resource/js/detectBrowser.js"></script>
+    <script src="./resource/js/header.js" defer></script>
+    <script src="./resource/js/registration.js" defer></script>
+    <script src="./resource/js/login.js" defer></script>
+    <script src="./resource/js/showMessage.js" defer></script>
+</head>
+<body>
+
+<?php require(__DIR__ . '/view/header.php'); ?>
+
+<div class="content" style="padding-top: 0">
+    <div class="cat_fold">
+        <a href="./index.php"> Главная </a>
+        <p><span> &#92; Заказы </span></p>
+    </div>
+    <h2> Ваши заказы </h2>
+
+
+</div>
+
+<?php require(__DIR__ . '/view/footer.php'); ?>
+
+</body>
+</html>
