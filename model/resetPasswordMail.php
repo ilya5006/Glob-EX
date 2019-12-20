11d3c048fe80b1752befb51694089e1bb61153a4
Alexnder <mrreads@yandex.com>
2019-12-20 22:45:22 +0300

    mail send

---
+++ model/resetPasswordMail.php
@@ -0,0 +1,11 @@
+﻿<?php
+$to = "mrreads@yandex.com";
+
+$subject = "тема";
+
+$txt = "текст!";
+
+$headers = "From: help@vsekanc.ru" . "\r\n" .
+"CC: help@vsekanc.ru";
+
+mail($to, $subject, $txt, $headers);
