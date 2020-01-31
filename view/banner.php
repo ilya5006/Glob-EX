9d9c79d4d8105ac3bd893a35f33d76d3953a722a
ilya5006 <samarcev.ilya@mail.ru>
2020-01-31 10:39:46 +0300

    Update banner.php

--- view/banner.php
+++ view/banner.php
@@ -1,7 +1,6 @@
 <div class="content">
     <div class="banners">
         <div class="buttons"></div>
-        <!-- короче, тут всем КРОМЕ 1 элемента надо добавить класс hide, ну это я когда в цикле php будешь выводить там -->
         <div class="banner"><img src="./resource/img/banners/banner1.png" alt="banner"><p>КАНЦЕЛЯРСКИЕ <br> ТОВАРЫ 1</p></div>
         <div class="banner hide"><img src="./resource/img/banners/banner1.png" alt="banner"><p>КАНЦЕЛЯРСКИЕ <br> ТОВАРЫ 2</p></div>
         <div class="banner hide"><img src="./resource/img/banners/banner1.png" alt="banner"><p>КАНЦЕЛЯРСКИЕ <br> ТОВАРЫ 3</p></div>
